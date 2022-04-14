466e2e3c57b5617fc2453dc26ba96ca93c559705
OleanjiKingCode <holamilean56@gmail.com>
2022-04-14 23:23:32 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x5f6ACccc002b388d5a524a6c703121320b5Ae05D";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
